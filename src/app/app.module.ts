e135b44c40343d4214abed3c37a489f264078619
Ricardo Belfort <rbelfort2004@gmail.com>
2020-11-16 13:19:51 -0400

    adding new components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewTransactionComponent } from './components/new-transaction/new-transaction.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTransactionComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
